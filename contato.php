13f7f76d6fad89f2b588f5c32cdb4b1b4854d303
mariliaferreira <mariliaferreiraa@gmail.com>
2015-04-14 16:11:21 -0300

    Contato

--- contato.php
+++ contato.php
@@ -38,21 +38,37 @@
           <h1>Contato</h1>
         </div>
         
-        <form>
-        Nome<br />
-        <input type="text" name="name">
-        <br />
-        <br />
-        Email<br />
-        <input type="text" name="email">
-        <br />
-        <br />
-        Mensagem<br />
-        <input type="textarea" name="mensagem">
-        <br />
+        <div class="col-md-4">
+        <h4><strong>Coordenação da Iniciação Científica</strong></h4>
+        <p><strong>Cleybe Vieira</strong><br />
+            cleybe.vieira@pucpr.br<br />
+            55(41)3271-2112 | 3271-1602<br />
+            Rua Imaculada Conceição, 1155<br />
+            Prédio Administrativo - 6º Andar Câmpus Curitiba<br />
+            Bairro Prado Velho - CEP 80215-901<br />
+            Curitiba - Paraná - Brasil</p>
+        </div>
 
-        </form>
-      
+        <div class="col-md-4">
+        <h4><strong>Coordenação de Intercâmbio Internacional e Cooperação</strong></h4>
+        <p>+55(41)3271-1556 | 3271-1697<br />
+        Rua Imaculada Conceição, 1155<br />
+        Prédio Administrativo - Térreo Câmpus Curitiba<br />
+        Bairro Prado Velho - CEP 80215-901<br />
+        Curitiba - Paraná - Brasil</p>
+        </div>
+
+        <div class="col-md-4" style="padding-bottom: 60px;">
+        <h4><strong>Administrativo</strong></h4>
+        <p><strong>Alessandra Lacerda</strong><br />
+            alessandra.lacerda@pucpr.br<br />
+            <br />
+            <strong>Valesca Walesko</strong><br />
+            intercambio@pucpr.br<br />
+            <br />
+            <strong>Lídia Maria Weretychi Kowalski</strong><br />
+            csf@pucpr.br</p>
+        </div>
 
 
       <!-- FOOTER -->
